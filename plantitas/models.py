c05b603e188341fd918140bc7dc5554e7fa5ea36
Romulo Tole <tolemaicor@gmail.com>
2023-04-28 16:20:27 -0400

    Creando la clase Planta en el modulo models

--- plantitas/models.py
+++ plantitas/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Planta(models.Model):
+    nombre = models.CharField(max_length=30)
+    tipo_planta = models.CharField(max_length=30)
+    enfermedad = models.BooleanField()
+    descripcion_enfermedad = models.CharField(max_length=500)
+    imagen_dir = models.CharField(max_length=250)
