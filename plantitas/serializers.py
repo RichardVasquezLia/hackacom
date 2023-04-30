c6b29713f68b856b5adfb949094636eedb3d1602
Romulo-Tolemaico <111012911+Romulo-Tolemaico@users.noreply.github.com>
2023-04-29 22:49:31 -0400

    Delete plantitas directory

--- plantitas/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-from plantitas.models import Planta
-
-class PlantitasSerializer(ModelSerializer):
-    class Meta:
-        model = Planta
-        fields = ('id','nombre','tipo_planta','enfermedad','descripcion_enfermedad','imagen_dir')
--- plantitas/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from plantitas.views import PlantasCreateView, PlantasUpdateDelete
-
-urlpatterns = [
-    path("api/plantitas/", PlantasCreateView.as_view()),
-    path("api/plantitas/<pk>/", PlantasUpdateDelete.as_view()),
-]
